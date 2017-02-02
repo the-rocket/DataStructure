165918a0c9620c3dedf9e44be0733be511cb7afa
Daniyar Kayirbolatov <daniyarkayirbolatov@gmail.com>
2017-02-01 20:44:21 -0600

    Assignment 1: Stack reverse input

---
+++ src/com/company/ReverseInputStack.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ReverseInputStack {
+
+    static Stack<String> StackWord;
+    private static Scanner scanner;
+    public static void main(String[] args) {
+	    scanner = new Scanner(System.in);
+        StackWord = new Stack<>();
+        for (int word = 1; word <= 10; word++) {
+            String newWord = scanner.next();
+            StackWord.push(newWord);
+        }
+        while (!StackWord.empty()) {
+            String WordFromEnd = StackWord.pop();
+            System.out.print(WordFromEnd + " ");
+        }
+    }
+}
